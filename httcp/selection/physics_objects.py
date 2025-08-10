9205c53eade7178cce3e26f574aa46d0e816242e
gourab saha <gourab.saha@cern.ch>
2025-08-11 00:02:23 +0200

    no ip-significance cut on e and mu for now

--- httcp/selection/physics_objects.py
+++ httcp/selection/physics_objects.py
@@ -82,7 +82,7 @@ def muon_selection(
         #"muon_iso_0p15"       : muons.pfRelIso04_all < 0.15, # For SR, moved to categorization
         "muon_iso_0p5"        : muons.pfRelIso04_all < 0.5, # new categorization following DESY setup 
         # not before applying correction from IPsig calibration
-        "muon_ipsig_1p0"      : np.abs(muons.IPsig) > 1.0,
+        #"muon_ipsig_1p0"      : np.abs(muons.IPsig) > 1.0,
     }
     single_veto_selections = {
         "muon_pt_10"          : muons.pt > 10,
@@ -227,7 +227,7 @@ def electron_selection(
         "electron_mva_iso_wp80"   : mva_iso_wp80 == 1,
         "electron_iso_0p15"       : electrons.pfRelIso03_all < 0.5,
         # not before applying correction from IPsig calibration        
-        "electron_ipsig_1p0"      : np.abs(electrons.IPsig) > 1.0,
+        #"electron_ipsig_1p0"      : np.abs(electrons.IPsig) > 1.0,
     }
     single_veto_selections = {
         "electron_pt_10"          : electrons.pt > 10,
